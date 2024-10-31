6bbfaf19f5a489aece856b853bcc4bcb358b518a
15Math <matheus.loureiro1215@gmail.com>
2024-10-31 12:10:03 -0300

    all aplication developed

--- backend/src/controllers/receiptController.js
+++ backend/src/controllers/receiptController.js
@@ -2,6 +2,8 @@ import fs from "fs/promises";
 import { PDFDocument } from "pdf-lib";
 import pdf from "pdf-parse/lib/pdf-parse.js"
 import path from "path";
+import archiver from "archiver";
+import { PassThrough } from "stream";
 
 const uploadDir = path.resolve("uploads"); 
 
@@ -14,7 +16,6 @@ const createGenericName = ()=>{
 }
 
 const setPdfName = async (filePath) => {
-
     const dataBuffer = await fs.readFile(filePath);
     const data = await pdf(dataBuffer);
     const pdfText = data.text;
@@ -31,41 +32,42 @@ const setPdfName = async (filePath) => {
 
     if(cleanFirstLine === "ComprovantedeTransferência"){
         
-        matchDate = pdfText.match(/data da transferência:\s*(\d{2}\/\d{2}\/\d{4})/i)[1];
+        matchDate = pdfText.match(/data da transferência:\s*(\d{2}\/\d{2}\/\d{4})/i)?.[1];
 
-        matchReceiverName = pdfText.match(/nome do recebedor:\s*([^\n]+)/i)[1];
+        matchReceiverName = pdfText.match(/nome do recebedor:\s*([^\n]+)/i)?.[1];
         
-        paymAmount = pdfText.match(/valor:\s*R\$\s*([\d.]+,\d{2})/i)[1];
+        paymAmount = pdfText.match(/valor:\s*R\$\s*([\d.]+,\d{2})/i)?.[1];
 
     }else if(cleanFirstLine === "Comprovantedepagamento-DARF"){
         
-        matchDate = pdfText.match(/data do pagamento:\s*(\d{2}\/\d{2}\/\d{4})/i)[1];
+        matchDate = pdfText.match(/data do pagamento:\s*(\d{2}\/\d{2}\/\d{4})/i)?.[1];
         
         receiverName = "DARF";
 
-        paymAmount = pdfText.match(/valor total:\s*R\$\s*([\d.]+,\d{2})/i)[1];
+        paymAmount = pdfText.match(/valor total:\s*R\$\s*([\d.]+,\d{2})/i)?.[1];
 
     }else if(cleanFirstLine === "Comprovantedepagamentodeboleto"){
         
-        matchDate = pdfText.match(/(\d{2}\/\d{2}\/\d{4})\s*Autenticação\s+mecânica/i)[1];
+        matchDate = pdfText.match(/(\d{2}\/\d{2}\/\d{4})\s*Autenticação\s+mecânica/i)?.[1];
         
-        matchReceiverName = pdfText.match(/Beneficiário:\s*([\w\s]+)(?=\s+CPF\/CNPJ)/i)[1];
+        matchReceiverName = pdfText.match(/Beneficiário:\s*([\w\s]+)(?=\s+CPF\/CNPJ)/i)?.[1];
         
         let matchPaymAmount;
         if(!pdfText.includes("Beneficiário Final:")){
-            matchPaymAmount = pdfText.match(/(?<!\d)(?<![A-Za-z/])(\d{1,3}(?:\.\d{3})*,\d{2})(?=\s*Data de pagamento:)/i)[1];
+            matchPaymAmount = pdfText.match(/(?<!\d)(?<![A-Za-z/])(\d{1,3}(?:\.\d{3})*,\d{2})(?=\s*Data de pagamento:)/i)?.[1];
+            paymAmount = matchPaymAmount.substring(2);
         }else{
-            matchPaymAmount = data.text.match(/(\d{1,3}(?:\.\d{3})*,\d{2})(?=\s*Beneficiário Final:)/i)[1];
+            paymAmount = data.text.match(/(\d{1,3}(?:\.\d{3})*,\d{2})(?=\s*Beneficiário Final:)/i)?.[1];
         }
         
-        paymAmount = matchPaymAmount.substring(2);
+        
 
     }else if(cleanFirstLine === "ComprovantedepagamentoQRCode"){
-        matchDate = pdfText.match(/data e hora da expiração:\s*(\d{2}\/\d{2}\/\d{4})\s*às\s*\d{2}:\d{2}:\d{2}/i)[1];  
+        matchDate = pdfText.match(/data e hora da expiração:\s*(\d{2}\/\d{2}\/\d{4})\s*às\s*\d{2}:\d{2}:\d{2}/i)?.[1];  
 
-        matchReceiverName = pdfText.match(/nome do recebedor:\s*([^\n]*)/i)[1];
+        matchReceiverName = pdfText.match(/nome do recebedor:\s*([^\n]*)/i)?.[1];
         
-        paymAmount = pdfText.match(/valor da transação:\s*([\d.]+,\d{2})/i)[1];
+        paymAmount = pdfText.match(/valor da transação:\s*([\d.]+,\d{2})/i)?.[1];
 
     }else{
         return createGenericName()
@@ -81,7 +83,7 @@ const setPdfName = async (filePath) => {
     }
 
     const pdfName = `${paymDate} - ${receiverName} - ${paymAmount} - Comprovante`
-
+    console.log(pdfName);
     return pdfName;
 };
 
@@ -98,7 +100,7 @@ const splitPdf = async (req, res) => {
         const existingPdfBytes = await fs.readFile(req.file.path);
         const pdfDoc = await PDFDocument.load(existingPdfBytes);
         const totalPages = pdfDoc.getPageCount();
-        const splitPdfPaths = [];
+        const splitPdfBuffers = [];
 
         // Loop para dividir o PDF
         for (let i = 0; i < totalPages; i++) {
@@ -106,26 +108,49 @@ const splitPdf = async (req, res) => {
             const [copiedPage] = await newPdfDoc.copyPages(pdfDoc, [i]);
             newPdfDoc.addPage(copiedPage);
 
-            const pdfBytes = await newPdfDoc.save();
+            let pdfBytes = await newPdfDoc.save();
+            pdfBytes = Buffer.from(pdfBytes);
+            console.log('Tipo de pdfBytes:', Buffer.isBuffer(pdfBytes));
+
             const outputPath = path.join(uploadDir, `split_page_${i+1}.pdf`);
-            
+
             await fs.writeFile(outputPath, pdfBytes); 
             console.log(`Arquivo criado: ${outputPath}`);
-            const newFileName = await setPdfName(outputPath);
-            const newPath = path.join(uploadDir, `${newFileName}.pdf`);
-            
-            await fs.rename(outputPath, newPath);
 
-            splitPdfPaths.push(newPath);
+            const newFileName = await setPdfName(outputPath);
+            splitPdfBuffers.push({ buffer: pdfBytes, fileName: `${newFileName}.pdf` });
+            
         }
-        res.json({ splitPdfs: splitPdfPaths });
+
+        const archive = archiver("zip", { zlib: { level: 9 } });
+        const passThrough = new PassThrough();
+
+        // Pipa o `archive` para o `PassThrough` e armazena os chunks
+        const zipBuffer = await new Promise((resolve, reject) => {
+            const zipChunks = [];
+            passThrough.on("data", (chunk) => zipChunks.push(chunk));
+            passThrough.on("end", () => resolve(Buffer.concat(zipChunks)));
+            passThrough.on("error", reject);
+
+            for (const file of splitPdfBuffers) {
+                archive.append(file.buffer, { name: file.fileName });
+            }
+            archive.pipe(passThrough);
+            archive.finalize();
+        });
+
+        const zipBase64 = zipBuffer.toString("base64");
+        res.json({ zipBase64 });
+
     } catch (error) {
         console.error('Erro ao ler o PDF:', error);
         res.status(500).json({ error: 'Erro ao processar o arquivo PDF.' });
     } finally {
         // Limpa o diretório de uploads
         await fs.rm(uploadDir, { recursive: true, force: true });
+        await fs.mkdir(uploadDir, { recursive: true });
         console.log("Diretório de uploads removido com sucesso.");
+        
     }
 };
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,93 @@
 import { useState } from 'react';
+import arrowImg from './assets/arrow.png';
+import folderImg from './assets/folder.png'
 
 import './App.css'
 
 
 function App() {
+  const [pdfSelected, setPdfSelected] = useState(false);
+  const [zip, setZip] = useState('')
 
-  const handleFileChange = ()=>{
+  const handleFileDownload = ()=> {
+    // Remove o prefixo "data:application/zip;base64," se presente
+    const base64WithoutPrefix = zip.replace(/^data:application\/zip;base64,/, '');
+    const byteCharacters = atob(base64WithoutPrefix);
+    
+    // Cria um array de bytes
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    
+    const blob = new Blob([byteArray], { type: 'application/zip' });
+    const url = URL.createObjectURL(blob);
+    
+    const link = document.createElement('a');
+    link.href = url;
+    const timestamp = Date.now(); 
+    const randomPart = Math.floor(Math.random() * 100000); 
+    link.download = "Comprovantes "+timestamp+randomPart;
+    
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    
+    URL.revokeObjectURL(url);
 
+    setPdfSelected(false);
+    setZip('');
+}
+
+
+  const handleFileChange = async(event)=>{
+    const pdfFile = event.target.files[0];
+    if (pdfFile) {
+      setPdfSelected(true); 
+      const formData = new FormData(); 
+      formData.append('pdfFile', pdfFile);
+      try {
+        const response = await fetch('http://localhost:3333/splitPdf', {
+          method: 'POST',
+          body: formData, // Envia o FormData
+        });
+        
+        if (!response.ok) {
+          throw new Error('Erro ao enviar o arquivo');
+        }
+  
+        const data = await response.json();
+        setZip(data.zipBase64);
+
+        console.log('Arquivo enviado com sucesso:', data);
+      } catch (error) {
+        console.error('Erro ao enviar o arquivo:', error);
+      }
+    }
   }
 
   const handleDrop = (event) => {
-    event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    if (file) {
-      document.getElementById("file-upload").files = event.dataTransfer.files;
-    }
+    event.preventDefault(); 
+    const file = event.dataTransfer.files[0]; 
+    const fileUpload = document.getElementById("file-upload");
     const dragScreen = document.getElementById("dragScreen");
-    dragScreen.style.opacity = 0;
-    dragScreen.style.zIndex = -1;
-  };
+
+    if (file && file.type === 'application/pdf') {
+        const dataTransfer = new DataTransfer(); 
+        dataTransfer.items.add(file); 
+        fileUpload.files = dataTransfer.files; 
+
+        handleFileChange({ target: fileUpload });
+
+        dragScreen.style.opacity = 0; 
+        dragScreen.style.zIndex = -1;  
+    } else {
+        alert('Por favor, solte um arquivo PDF.');
+        dragScreen.style.opacity = 0;
+    }
+    
+  }
 
   const handleDragOver = (event) => {
     event.preventDefault();
@@ -39,21 +108,44 @@ function App() {
         Solte seu PDF aqui
       </div>
       <header>
-        Nomeador de Comprovantes
+        {pdfSelected ? (
+          <button id="arrow" onClick={()=>{setPdfSelected(false); setZip('')}}>
+            <img src={arrowImg} alt="voltar"/>
+          </button>
+        ): null}
+        <p className='header-title'>RECEIPT NAMER</p>
       </header>
+      {!pdfSelected ? (
       <section>
         <h1>Nomear comprovantes</h1>
         <p className='description'>Divida seu arquivo PDF de comprovantes em uma pasta zip com arquivos individuais nomeados com suas informações.</p>
-        <label htmlFor="file-upload" className="custom-file-upload">
+        <label htmlFor="file-upload" className="custom-file">
             Selecione ou arraste o arquivo PDF
         </label>
         <input 
           id="file-upload"
           type="file"
           onChange={handleFileChange}
+          name="pdfFile"
+          accept=".pdf,application/pdf"
         />
         <p>Modelos nomeados: Itaú Transferência, Itaú DARF, Itaú Boleto, Itaú QR Code</p>
       </section>
+
+       ) : (
+
+        <section>
+        <h1>Os comprovantes foram nomeados!</h1>
+        <p>Clique para baixar o zip com os seus PDFs</p>
+        <button 
+          className="custom-file"
+          onClick={handleFileDownload}
+        >
+          <img id="folderImg"src={folderImg} alt=""/>
+          Baixar a comprovantes
+        </button>
+      </section>
+      )}
       
     </>
   )
