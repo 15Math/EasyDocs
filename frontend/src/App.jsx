5e124163f3a755fa05fdcc55b332290f1d2ed17e
15Math <matheus.loureiro1215@gmail.com>
2024-10-30 23:31:02 -0300

    main screen parcially done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleFileChange = ()=>{
+
+  }
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    if (file) {
+      document.getElementById("file-upload").files = event.dataTransfer.files;
+    }
+    const dragScreen = document.getElementById("dragScreen");
+    dragScreen.style.opacity = 0;
+    dragScreen.style.zIndex = -1;
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    const dragScreen = document.getElementById("dragScreen");
+    dragScreen.style.opacity = 100;
+    dragScreen.style.zIndex = 1;
+  };
+
+  const handleDragLeave = ()=>{
+    const dragScreen = document.getElementById("dragScreen");
+    dragScreen.style.opacity = 0;
+    dragScreen.style.zIndex = -1;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="dragScreen" onDrop={handleDrop} onDragOver={handleDragOver} onDragLeave={handleDragLeave}>
+        Solte seu PDF aqui
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        Nomeador de Comprovantes
+      </header>
+      <section>
+        <h1>Nomear comprovantes</h1>
+        <p className='description'>Divida seu arquivo PDF de comprovantes em uma pasta zip com arquivos individuais nomeados com suas informações.</p>
+        <label htmlFor="file-upload" className="custom-file-upload">
+            Selecione ou arraste o arquivo PDF
+        </label>
+        <input 
+          id="file-upload"
+          type="file"
+          onChange={handleFileChange}
+        />
+        <p>Modelos nomeados: Itaú Transferência, Itaú DARF, Itaú Boleto, Itaú QR Code</p>
+      </section>
+      
     </>
   )
 }
