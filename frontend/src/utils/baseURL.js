bd720b2115e0177e6b704c78fde499705e224592
15Math <matheus.loureiro1215@gmail.com>
2024-11-08 11:54:48 -0300

    varias coisas

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const baseURL = "https://easy-docs-api.vercel.app"
 //URL para desenvolvimento
-// const baseURL = "http://localhost:3000"
+//const baseURL = "http://localhost:3000"
 
 export default baseURL;
