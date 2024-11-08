fe08e29d368021ed2c1e81852b8a1222fcc2e0ad
15Math <matheus.loureiro1215@gmail.com>
2024-11-08 15:49:18 -0300

    new start src

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,6 @@ import app from './app.js'
 dotenv.config()
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>console.log('Server running on port: http://localhost:' + PORT))
+app.listen(PORT,()=>console.log('Server running on port: ' + PORT))
 
 export default app; 
