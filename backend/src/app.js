54de111402fc1809333d787ad949a64a95be5812
15Math <matheus.loureiro1215@gmail.com>
2024-11-08 10:23:32 -0300

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,7 @@ import router from "./router.js"
 import cors from "cors"
 const app = express();
 
-
 app.use(express.json())
-app.use(cors({
-    origin: 'https://easydocs15.netlify.app'
-}));
+app.use(cors());
 app.use(router);
 export default app;
