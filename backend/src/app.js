e4116a6b8e0ad8a643d6cfd1e0b680227685f55c
15Math <matheus.loureiro1215@gmail.com>
2024-11-08 12:37:39 -0300

    ignore TT error

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,15 @@ import cors from "cors"
 const app = express();
 
 app.use(express.json())
+
+process.on('warning', (warning) => {
+    if (warning.name === 'Warning' && warning.message.includes('TT: undefined function: 32')) {
+      return;
+    }
+    console.warn(warning.name, warning.message, warning.stack);
+  });
+  
+
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'OPTIONS'],
